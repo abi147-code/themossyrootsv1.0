8ebc43c45f7a0877b850ff1190463f6e8cda1bf3
Abishek Elangeswaran <abishek147ae@gmail.com>
2026-01-23 15:28:22 +0100

    Save ongoing changes

--- services/web/src/app/layout.tsx
+++ services/web/src/app/layout.tsx
@@ -56,7 +56,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TKWHWL9T');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${manrope.variable} ${geistMono.variable} bg-[#0B0F14] text-[#F5F7F9] antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKWHWL9T"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
