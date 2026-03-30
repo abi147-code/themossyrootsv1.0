929c165c04b45bf2e074a108b6d6de5a5daf1e4a
abishek <abishek147ae@gmail.com>
2026-03-30 23:18:05 +0200

    chore(web): normalize demo blog file

--- services/web/src/app/[locale]/demo/[client-slug]/blog/page.tsx
+++ services/web/src/app/[locale]/demo/[client-slug]/blog/page.tsx
@@ -1,4 +1,4 @@
-﻿import { cookies } from 'next/headers';
+import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import MolossExperience from '../moloss/DemoExperience';
